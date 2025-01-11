b4366d8fec4c284bb040e6e793f5878c4676926b
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-01-11 19:05:56 -0300

    docs: fix replace-pkg-version with part titles

--- docs/book/bin/replace-pkg-version.sh
+++ docs/book/bin/replace-pkg-version.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 PKG_VERSION=$(cat "$(dirname -- "$0")/../../../typst.toml" | grep version | sed -e 's/version = "\|"//g')
-jq .[1] | jq '.sections[].Chapter.content |= gsub("@preview/elembic:X.X.X"; "@preview/elembic:'$PKG_VERSION'")' | jq '.sections[].Chapter.sub_items[].Chapter.content |= gsub("@preview/elembic:X.X.X"; "@preview/elembic:'$PKG_VERSION'")'
+
+# .[1] => take just the second item of the input array, which has '.sections'
+# Next, for each section, if it isn't a Chapter (e.g. PartTitle) then it is kept as is through '.'
+# Oterwise, we replace "@preview/elembic:X.X.X" with "@preview/elembic:VERSION" in both the top-level
+# chapter's as well as any sub-chapter's contents.
+jq .[1] | jq '.sections[] |= (if .Chapter? == null then . else (.Chapter.content, .Chapter.sub_items[].Chapter.content) |= gsub("@preview/elembic:X.X.X"; "@preview/elembic:'$PKG_VERSION'") end)'
