5676de17517923ac0a30bdd395f299f77f1113e3
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-01-12 19:17:08 -0300

    docs/bin: use @local

--- docs/book/bin/replace-pkg-version.sh
+++ docs/book/bin/replace-pkg-version.sh
@@ -6,4 +6,4 @@ PKG_VERSION=$(cat "$(dirname -- "$0")/../../../typst.toml" | grep version | sed 
 # Next, for each section, if it isn't a Chapter (e.g. PartTitle) then it is kept as is through '.'
 # Oterwise, we replace "@preview/elembic:X.X.X" with "@preview/elembic:VERSION" in both the top-level
 # chapter's as well as any sub-chapter's contents.
-jq .[1] | jq '.sections[] |= (if .Chapter? == null then . else (.Chapter.content, .Chapter.sub_items[].Chapter.content) |= gsub("@preview/elembic:X.X.X"; "@preview/elembic:'$PKG_VERSION'") end)'
+jq .[1] | jq '.sections[] |= (if .Chapter? == null then . else (.Chapter.content, .Chapter.sub_items[].Chapter.content) |= gsub("@preview/elembic:X.X.X"; "@local/elembic:'$PKG_VERSION'") end)'
